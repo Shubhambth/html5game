cfd6c04981879752a59b82bdbc5e766388092db0
Shubham Singh <164128999+Shubhambth@users.noreply.github.com>
2025-04-16 14:55:00 +0000

    v6

--- core/views.py
+++ core/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
-
+def allgames(request):
+    return render(request, "html5allgames.html")
 
 def page(request):
     return render(request, 'page.html')
