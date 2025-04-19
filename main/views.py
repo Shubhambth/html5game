a806e2df0e5636ad3ccc57a6b646f3214c5f98c2
Shubham Singh <164128999+Shubhambth@users.noreply.github.com>
2025-04-19 12:51:07 +0000

    v10.1

--- main/views.py
+++ main/views.py
@@ -24,11 +24,14 @@ def detailpage2(request,id):
     game.save()
     popular_games = Game.objects.order_by('-views_count')[:5]
     posts = Post.objects.all()
-    return render(request, 'detailpage2.html',{'game': game,'allgames':allgames,'popular_games': popular_games,'posts': posts})
+    return render(request, 'detailpage2.html',{'game': game,'allgames':allgames,'popular_games':popular_games,'posts': posts})
 
 def blog(request):
     posts = Post.objects.all()
-    return render(request, 'blog.html',{'blogs': posts})
+    popularpost = Post.objects.order_by('-views')[:1]
+    topapp = Post.objects.filter(categories__name='Top App').order_by('-views')[:1]
+    topgame = Post.objects.filter(categories__name='Top Games').order_by('-views')[:2]
+    return render(request, 'blog.html',{'blogs': posts,'popularpost': popularpost,'topapp':topapp,'topgame':topgame})
 
 
 def allgames(request):
